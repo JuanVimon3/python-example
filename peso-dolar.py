2c594b1e1ad03d70e594b37da50d3435dd82938f
Juan <jvillamonsalve@gmail.com>
2022-08-10 23:42:15 -0500

    initial commit

---
+++ peso-dolar.py
@@ -0,0 +1,17 @@
+
+def cambiar_divisas(pesos_colombianos, eleccion)
+
+
+    if eleccion== "US":
+        dolares= round((pesos_colombianos/4200.89),2)
+        print(str(dolares) + " " + eleccion)
+    elif eleccion == "YEN":
+        yenes= round((pesos_colombianos/4000.87),2)
+        print(str(yenes)  + " " + eleccion)
+    elif eleccion == "CHP":
+        chilenos= round((pesos_colombianos/430.67),2)
+        print(str(chilenos)  + " " + eleccion)
+    else:
+        print("SELECCION INVÁLIDA")
+
+cambiar_divisas(2000000, "US")
